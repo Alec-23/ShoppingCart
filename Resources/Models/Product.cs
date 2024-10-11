71d86bc89045e57f33e4538a36f44d885d98a54d
Alec Christensen <alec_christensen@hotmail.com>
2024-10-12 00:05:32 +0200

    Skapat Product-klassen med egenskaper för ID, namn och pris

---
+++ Resources/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Resources.Models;
+
+internal class Product
+{
+    public Guid ID { get; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+
+    public Product(string name, decimal price)
+    {
+        ID = Guid.NewGuid();
+        Name = name;
+        Price = price;
+    }
+}
