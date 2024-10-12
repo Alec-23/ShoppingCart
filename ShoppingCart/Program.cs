eacc415eda204c30982a7415ac55e5bd931bfd44
Alec Christensen <alec_christensen@hotmail.com>
2024-10-12 16:18:56 +0200

    Skapade en instans av Menu-klassen för att kunna starta menyn för testning

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using ShoppingCart.Menus;
+
+namespace ShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            {
+                menu.ShowMenu();
+            }
+        }
+    }
+}
+
+
