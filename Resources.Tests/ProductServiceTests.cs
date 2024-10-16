09888e20d8718df973bd56c1ba85255285eab8ab
Alec Christensen <alec_christensen@hotmail.com>
2024-10-16 12:09:57 +0200

    Skapat enhetstest med xUnit för att verifiera att produkter kan läggas till i listan

---
+++ Resources.Tests/ProductServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Resources.Services;
+
+namespace Resources.Tests;
+
+public class ProductServiceTests
+{
+    [Fact]
+    public void AddProduct_ShouldIncreaseProductCount()
+    {
+        //Arange
+        var productService = new ProductService();
+        int initialCount = productService.GetAllProducts().Count;
+
+        //Act
+        productService.AddProduct("TestProduct", 9.99m);
+        int finalCount = productService.GetAllProducts().Count;
+
+        //Assert
+        Assert.Equal(initialCount + 1, finalCount);
+    }
+}
